2b03f26437f660c9cbf2bb2da84ca38758892970
Christian Tirol <criickyO@gmail.com>
2015-04-23 21:39:14 -0400

    Update streaming_with_tweepy.py

--- streaming_with_tweepy.py
+++ streaming_with_tweepy.py
@@ -6,10 +6,10 @@ from bs4 import BeautifulSoup as bs
 import pandas as pd
 import sched, time
 
-ckey = '4IZDtQJKv0YdwMNdXETZnSq9Y'
-csecret = 'ccD3DHRsXLSWXMr1V9vZ3dspF8KPvfWLTFB4Kvk21zk3inMeTi'
-atoken = '262034478-mwhM0KKB0h6M45IxWyCmpCAvwj1rkxjiRl6ArANy'
-asecret = 'y75xQdvPoWIibYmM9i4ZkjEUk5x7xCPdqHg0mErbUMCQJ'
+ckey = 'ckey'
+csecret = 'csecret'
+atoken = 'atoken'
+asecret = 'asecret'
 
 class listener(StreamListener):
 
